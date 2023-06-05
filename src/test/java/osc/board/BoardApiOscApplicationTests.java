b4a39410559051a56182d22c650b6d0047f9b591
hataerin <goil1113@likelion.org>
2023-06-05 10:01:04 +0900

    application.properties -> application.yml로 변경

--- src/test/java/osc/board/BoardApiOscApplicationTests.java
+++ src/test/java/osc/board/BoardApiOscApplicationTests.java
@@ -1,13 +1,13 @@
-package osc.board;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BoardApiOscApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package osc.board;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BoardApiOscApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
